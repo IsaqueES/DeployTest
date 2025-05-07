e22b3f3d2458ab17b35d53557feaf0a13f2a026e
Isaq <isaqueestolanossi04@gmail.com>
2025-05-07 12:26:07 -0300

    refatorar: remover dependência não utilizada do pg no package.json e ajustar indentação no app.js

--- DeployTest/app.js
+++ DeployTest/app.js
@@ -59,6 +59,5 @@ app.post("/contatos", async (req, res) => {
 // Iniciar o servidor na porta 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Servidor rodando na porta " + PORT);
+    console.log("Servidor rodando na porta " + PORT);
 });
-
